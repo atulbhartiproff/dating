4cbfa90596bcddaee5e9cc6209877f1861c0c963
atulbhartiproff <atulbhartiproff@gmail.com>
2025-06-11 22:45:43 +0530

    Adding Fredoka custom font

--- app/tabs/app.tsx
+++ app/tabs/app.tsx
@@ -8,13 +8,17 @@ const RemoteBackgroundScreen = () => {
       style={styles.background}
       resizeMode="cover"
     >
-      {/* <View style={styles.overlay}>
-        <Text style={styles.title}>Welcome</Text>
-      </View> */}
+      <View style={styles.overlay}>
+        <Text style={styles.fredokaBold}>Welcome</Text>
+      </View>
     </ImageBackground>
   );
 };
 
+
+
+
+//STYLES SECTION: Currently using Fredoka
 const styles = StyleSheet.create({
   background: {
     flex: 1,
@@ -30,6 +34,27 @@ const styles = StyleSheet.create({
     fontSize: 28,
     fontWeight: 'bold',
   },
-});
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  fredokaLight: {
+    fontFamily: 'Fredoka-VariableFont_wdth,wght',
+    fontWeight: '300',
+    fontSize: 24,
+  },
+  fredokaRegular: {
+    fontFamily: 'Fredoka-VariableFont_wdth,wght',
+    fontWeight: '400',
+    fontSize: 24,
+  },
+  fredokaBold: {
+    fontFamily: 'Fredoka-VariableFont_wdth,wght',
+    fontWeight: '700',
+    fontSize: 24,
+  },
+}
+);
 
 export default RemoteBackgroundScreen;
